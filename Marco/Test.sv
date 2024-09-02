f7020d2c0c815e31127f11883d62e07fe40e9063
Marco Umana <marcoan3108@gmail.com>
2024-09-02 14:41:50 -0600

    debug en el test

--- Marco/Test.sv
+++ Marco/Test.sv
@@ -14,7 +14,7 @@ class test #(parameter width = 16, parameter depth = 8);
     //definicion de laa interface a la que se conecta el DUT
     virtual fifo_if #(.width(width)) _if;
 
-    function new:
+    function new;
         test_sb_mbx = new();
         test_agent_mbx = new();
 
